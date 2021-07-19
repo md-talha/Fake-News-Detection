f9aab79d111947fd30c66435a72c5beb5f206ed0
md-talha <66055648+md-talha@users.noreply.github.com>
2021-07-19 23:34:41 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class AddNews(models.Model):
-    date = models.DateField()
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    url = models.URLField()
--- urls.py
+++ urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-	path('',views.index, name='index'),
-	path('insert',views.insert, name='insert'),
-	path('alogin',views.alogin, name='alogin'),
-	
-	
+	path('register',views.register, name='register'),
+	path('login',views.login, name='login'),
+	path('logout',views.logout, name='logout'),
+	path('home', views.home, name='home'),
+	path('test', views.test, name='test'),
 ]
