8b910f0ae0f0939b2554791cbbe10917cbf0887b
md-talha <66055648+md-talha@users.noreply.github.com>
2021-07-19 23:32:07 +0530

    Add files via upload

---
+++ chart/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import When
+from adint.models import AddNews
+# Create your views here.
+
+
+
+
+
+
+
+
+
+    
+    
